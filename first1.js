4a08514dafdd72893e545de5ad3f4e0defcdd48a
SatinderK <kainthsatinder8@gmail.com>
2021-03-12 22:31:52 -0800

    algo example

---
+++ first1.js
@@ -0,0 +1,19 @@
+// Print out all odd numbers from 1 to 20
+
+
+for(var i=1; i<=20;i++){
+    if(i%2!==0)
+    {
+        console.log(`the value of i is ${i}`);
+    }
+}
+
+
+
+// Sum and Print 1-5
+
+var sum=0;
+for(var i=1; i<=5;i++){
+sum=sum+i;
+    console.log( `Num: ${i}, Sum: ${sum}`)
+}
